98b6355e618cfabb7ae365775198848f5e8e4ace
Jó Oliveira <joypinheiro63@gmail.com>
2021-05-09 10:58:17 -0300

    Api-Comics-Marvel

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Comics from './Pages/Comics/index';
+import GlobalStyle from './Styles/global'
+const App: React.FC = () => {
+  return (
+    <>
+      <Comics />
+      <GlobalStyle />
+    </>
+  );
+}
+
+export default App;
